aaab933d4ca47e810506bdaf90c041971c0ad530
Kyle McKee <mckee.kyle@gmail.com>
2014-07-19 10:02:38 -0400

    Refactoring

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,7 +1,9 @@
+run = [fizzbuzz x | x <- [1..100]]
+
 fizzbuzz x
-  | x `is_divisible_by` 3 && x `is_divisible_by` 5 = "fizzbuzz"
-  | x `is_divisible_by` 3 = "fizz"
+  | x `is_divisible_by` 15 = "fizzbuzz"
   | x `is_divisible_by` 5 = "buzz"
+  | x `is_divisible_by` 3 = "fizz"
   | otherwise = show x 
 
 is_divisible_by x y = rem x y == 0
