743801c3bd1a78bd5a4db455702f3951bc2c2702
Kyle McKee <mckee.kyle@gmail.com>
2014-07-19 09:51:27 -0400

    Switched to use infix functions and guards

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,12 +1,7 @@
-fizzbuzz x = if divisible_by_three x && divisible_by_five x then
-                 "fizzbuzz"
-             else if divisible_by_three x then
-                "fizz"
-             else if divisible_by_five x then
-                "buzz"
-             else
-                show x 
+fizzbuzz x
+  | x `is_divisible_by` 3 && x `is_divisible_by` 5 = "fizzbuzz"
+  | x `is_divisible_by` 3 = "fizz"
+  | x `is_divisible_by` 5 = "buzz"
+  | otherwise = show x 
 
-divisible_by_three x = rem x 3 == 0
-
-divisible_by_five x = rem x 5 == 0
+is_divisible_by x y = rem x y == 0
